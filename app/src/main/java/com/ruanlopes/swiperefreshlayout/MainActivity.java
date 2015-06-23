b1cadd658a21b39a3eddf42fd418aa39a3b178d8
Ruan_Lopes <Ruan.lopes@gmail.com>
2015-06-23 12:04:12 -0400

    go!

--- app/src/main/java/com/ruanlopes/swiperefreshlayout/MainActivity.java
+++ app/src/main/java/com/ruanlopes/swiperefreshlayout/MainActivity.java
@@ -1,38 +1,140 @@
 package com.ruanlopes.swiperefreshlayout;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.ruanlopes.swiperefreshlayout.app.MyApplication;
+import com.ruanlopes.swiperefreshlayout.helper.Movie;
+import com.ruanlopes.swiperefreshlayout.helper.SwipeListAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private String URL_TOP_250 = "http://api.androidhive.info/json/imdb_top_250.php?offset=";
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ListView listView;
+    private SwipeListAdapter adapter;
+    private List<Movie> movieList;
+
+    // initially offset will be 0, later will be updated while parsing the json
+    private int offSet = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+
+        movieList = new ArrayList<>();
+        adapter = new SwipeListAdapter(this, movieList);
+        listView.setAdapter(adapter);
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        /**
+         * Showing Swipe Refresh animation on activity create
+         * As animation won't start on onCreate, post runnable is used
+         */
+        swipeRefreshLayout.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        swipeRefreshLayout.setRefreshing(true);
+
+                                        fetchMovies();
+                                    }
+                                }
+        );
+
     }
 
+    /**
+     * This method is called when swipe refresh is pulled down
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onRefresh() {
+        fetchMovies();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Fetching movies json by making http call
+     */
+    private void fetchMovies() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // showing refresh animation before making http call
+        swipeRefreshLayout.setRefreshing(true);
 
-        return super.onOptionsItemSelected(item);
+        // appending offset to url
+        String url = URL_TOP_250 + offSet;
+
+        // Volley's json array request object
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        if (response.length() > 0) {
+
+                            // looping through json and adding to movies list
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    JSONObject movieObj = response.getJSONObject(i);
+
+                                    int rank = movieObj.getInt("rank");
+                                    String title = movieObj.getString("title");
+
+                                    Movie m = new Movie(rank, title);
+
+                                    movieList.add(0, m);
+
+                                    // updating offset value to highest value
+                                    if (rank >= offSet)
+                                        offSet = rank;
+
+                                } catch (JSONException e) {
+                                    Log.e(TAG, "JSON Parsing error: " + e.getMessage());
+                                }
+                            }
+
+                            adapter.notifyDataSetChanged();
+                        }
+
+                        // stopping swipe refresh
+                        swipeRefreshLayout.setRefreshing(false);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Server Error: " + error.getMessage());
+
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+                // stopping swipe refresh
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        // Adding request to request queue
+        MyApplication.getInstance().addToRequestQueue(req);
     }
 }
---
+++ app/src/main/java/com/ruanlopes/swiperefreshlayout/helper/Movie.java
@@ -0,0 +1,18 @@
+package com.ruanlopes.swiperefreshlayout.helper;
+
+/**
+ * Created by Ruan_Lopes on 15-06-23.
+ */
+
+public class Movie {
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+}
